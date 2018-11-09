4b9def565d6e8ec8b6e9783257e7189d01fe7da4
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-11-09 20:57:13 +0200

    Create sign in/up UI
     * Create sign in/up window using MaterialDesignPackage.
     * Switch between sign in/up pages using Flipping card.
     * Use multiple frames to switch between sign up pages.

---
+++ FitnessApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FitnessApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void signInButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void createAnAccount_Click(object sender, RoutedEventArgs e)
+        {
+            signUpPages.NavigationService.Navigate(new SignUpPages.Page1());
+
+        }
+    }
+}
