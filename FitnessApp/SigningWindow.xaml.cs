19ab04df21bfb9ec811f32deeb0e41d45f8854a4
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-11-10 08:30:30 +0200

    Add Fonts directory & Rename MainWindow
     * Add Product Sans and Roboto fonts to Fonts directory.
     * Add FontFamilies to App.xaml <Application.Resources> tag.
     * Rename MainWindow to SigningWindow.
     * Deleted BG2.jpg permanently from Resources.

--- FitnessApp/SigningWindow.xaml.cs
+++ FitnessApp/SigningWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace FitnessApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SigningWindow : Window
     {
-        public MainWindow()
+        public SigningWindow()
         {
             InitializeComponent();
         }
