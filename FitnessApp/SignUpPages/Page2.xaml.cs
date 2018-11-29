0239186e13b1033a7c6623d7e2f65a3d50d7062a
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-11-29 21:55:20 +0200

    Remove all unnecessary Headers and Namespaces

--- FitnessApp/SignUpPages/Page2.xaml.cs
+++ FitnessApp/SignUpPages/Page2.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FitnessApp.SignUpPages
 {
@@ -30,12 +19,12 @@ namespace FitnessApp.SignUpPages
 
         private void SignUpButton_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new SignUpPages.Page3());
+            NavigationService.Navigate(new SignUpPages.Page3());
 
             //Change Back Card Header
-            FitnessApp.SigningWindow.SigningWindowObject.BackArrowButton.Visibility = Visibility.Hidden;
-            FitnessApp.SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Text = "Set up your Profile";
-            FitnessApp.SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Margin = new Thickness(-15);
+            SigningWindow.SigningWindowObject.BackArrowButton.Visibility = Visibility.Hidden;
+            SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Text = "Set up your Profile";
+            SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Margin = new Thickness(-15);
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
