fb82b3605b4c8f91b7e39a338fbc534a282a9074
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-11-30 23:08:17 +0200

    Add ChallengesViewModel.cs View Model Class
     * Create a view model calss for challenges page and initialize it with
       static temproray arguments.
     * ChallengeItems array will be initialized later from Database.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

---
+++ FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace FitnessApp.ViewModels
+{
+    class ChallengesViewModel
+    {
+        public ChallengesViewModel()
+        {
+            ChallengeItems = new[] {
+            new ChallengeItem("Challenge1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge2","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge3","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge4","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge5","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories")};
+        }
+
+        public ChallengeItem[] ChallengeItems { get; }
+    }
+}
