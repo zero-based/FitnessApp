d3fecdde29defbfc109e5f5525858517fbfcb433
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-12-01 05:25:56 +0200

    Highlight the chosen page from Side Menu Drawer
     * Add HomePage Navigation code.
     * Close Side Menu Drawer before navigation for a faster animation.
     * Create HighlightItem function to reset the current Highlighted Item
       and highlight a new item.

--- FitnessApp/UserMainWindow.xaml.cs
+++ FitnessApp/UserMainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace FitnessApp
 {
@@ -15,28 +16,68 @@ namespace FitnessApp
 
         private void UserMainWindowPagesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Navigate to selected Page
+            // Close Side Menu Drawer
+            SideMenuDrawer.IsLeftDrawerOpen = false;
+
+            // Navigate to the selected Page and Highlight the chosen Item
             switch (UserMainWindowPagesListBox.SelectedIndex)
             {
                 case 0:
-                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.ChallengesPage.ChallengesPageObject);
+                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.HomePage.HomePageObject);
+                    HighlightItem(HomeTextBlock, HomeIcon);
                     break;
 
                 case 1:
-                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.FitnessPlansPage.FitnessPlansPageObject);
+                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.ChallengesPage.ChallengesPageObject);
+                    HighlightItem(ChallengesTextBlock, ChallengesIcon);
                     break;
 
                 case 2:
-                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.CaloriesCalculatorPage.CaloriesCalculatorPageObject);
+                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.FitnessPlansPage.FitnessPlansPageObject);
+                    HighlightItem(FitnessPlansTextBlock, FitnessPlansIcon);
                     break;
 
                 case 3:
+                    UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.CaloriesCalculatorPage.CaloriesCalculatorPageObject);
+                    HighlightItem(CaloriesCalculatorTextBlock, CaloriesCalculatorIcon);
+                    break;
+
+                case 4:
                     UserWindowPagesFrame.NavigationService.Navigate(UserMainWindowPages.SettingsPage.SettingsPageObject);
+                    HighlightItem(SettingsTextBlock, SettingsIcon);
                     break;
             }
+        }
 
-            // Close Drawer after Navigation
-            SideMenuDrawer.IsLeftDrawerOpen = false;
+        public void HighlightItem(TextBlock _pageTextBlock, MaterialDesignThemes.Wpf.PackIcon _pageIcon)
+        {
+            // Set all Items' Foreground to Black 
+
+            // Home Item
+            HomeTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            HomeIcon     .Foreground = new SolidColorBrush(Colors.Black);
+
+            // Challenges Item
+            ChallengesTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            ChallengesIcon     .Foreground = new SolidColorBrush(Colors.Black);
+
+            // Challenges Item
+            FitnessPlansTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            FitnessPlansIcon     .Foreground = new SolidColorBrush(Colors.Black);
+
+            // Challenges Item
+            CaloriesCalculatorTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            CaloriesCalculatorIcon     .Foreground = new SolidColorBrush(Colors.Black);
+
+            // Challenges Item
+            SettingsTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+            SettingsIcon     .Foreground = new SolidColorBrush(Colors.Black);
+
+
+
+            // Highlight the required Item only
+            _pageTextBlock.Foreground = (Brush)Application.Current.Resources["PrimaryHueDarkBrush"];
+            _pageIcon     .Foreground = (Brush)Application.Current.Resources["PrimaryHueDarkBrush"];
         }
     }
 }
