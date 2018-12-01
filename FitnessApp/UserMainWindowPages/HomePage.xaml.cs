6fc4a11d2f92ca074b939b9e416a2a5cefda2f6b
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-12-01 05:23:23 +0200

    Add HomePage to UserMainWindowPages

---
+++ FitnessApp/UserMainWindowPages/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace FitnessApp.UserMainWindowPages
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public static HomePage HomePageObject = new HomePage();
+
+        public HomePage()
+        {
+            InitializeComponent();
+            HomePageObject = this;
+        }
+    }
+}
