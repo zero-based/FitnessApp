7211fae1fdb5ee541b5744b42e7d870155cb75ca
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-02 22:22:26 +0200

    Add PlansViewModel.cs View Model
     * Create a view model calss for plans page and initialize it with
       static temproray arguments.
     * PlanItems array will be initialized later from Database.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

---
+++ FitnessApp/ViewModels/PlansViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FitnessApp.ViewModels
+{
+    class PlansViewModel
+    {
+        public PlansViewModel()
+        {
+            PlanItems = new[] {
+        new PlanItem("Plan1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+            " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM"),
+
+        new PlanItem("Plan2","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM"),
+
+        new PlanItem("Plan3","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM")};
+        }
+        public PlanItem[] PlanItems { get; }
+    }
+}
+
+    
+
