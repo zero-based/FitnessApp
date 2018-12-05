e3199273d5b273d3379943b4f4b15577283c9c86
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-05 23:32:37 +0200

    Add Days View Model
     * Create a view model class for days and initialize it with
       static temporary arguments.
     * DayItems array will be initialized later from Database depending on
       the selected Plan ID.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

---
+++ FitnessApp/ViewModels/DaysViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace FitnessApp.ViewModels
+{
+    class DaysViewModel
+    {
+        public DaysViewModel(int planNumber)
+        {
+            //Initialize 30 Days of Plan of ID: planNumber + 1 in DayItems Array
+
+            DayItems = new[] {
+            new DayItem("Day1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli"),
+
+            new DayItem("Day2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli"),
+
+            new DayItem("Day3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli"),
+
+            new DayItem("Day4", "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli"),
+
+           new DayItem("Day5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing eli"),
+        };
+        }
+
+
+        public DayItem[] DayItems { get; }
+    }
+
+}
