b4736f1c997044be97befab39e710e24eab12adc
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-06 00:22:09 +0200

    Modify Challenge Page View
     * Add x:Name Attribute to join Challenge Button.
     * Get the index of the selected Challnge so we can use it to Modify
       Joined Challenges in Database Late on.

--- FitnessApp/UserMainWindowPages/ChallengesPage.xaml.cs
+++ FitnessApp/UserMainWindowPages/ChallengesPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace FitnessApp.UserMainWindowPages
 {
@@ -9,11 +11,20 @@ namespace FitnessApp.UserMainWindowPages
     {
         public static ChallengesPage ChallengesPageObject = new ChallengesPage();
 
+        int selectedChallengeIndex;
+
         public ChallengesPage()
         {
             InitializeComponent();
             ChallengesPageObject = this;
             DataContext = new ViewModels.ChallengesViewModel();
+        }
+
+        private void JoinChallengeButton_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //use selectedChallengeIndex + 1 as Challenge ID while using database
+            ToggleButton toggleButton = sender as ToggleButton;
+            selectedChallengeIndex = ChallengesListBox.Items.IndexOf(toggleButton.DataContext);
         }
     }
 }
