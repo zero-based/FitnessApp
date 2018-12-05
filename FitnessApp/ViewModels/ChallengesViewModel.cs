225ecc0f68a3fc41fa0a91050377a84c9dbae9b5
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-06 01:57:45 +0200

    Modify Challenges View Model
     * Add two Functions; one to Initialize allChallenges Array and the
       other to Initialize joinedChallenges Array.
     * Add two Properties to get Both arrays.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- FitnessApp/ViewModels/ChallengesViewModel.cs
+++ FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -2,9 +2,14 @@
 {
     class ChallengesViewModel
     {
-        public ChallengesViewModel()
+        private ChallengeItem[] joinedChallengeItems;
+        private ChallengeItem[] allChallengeItems;
+
+        public ChallengesViewModel() { }
+
+        public void JoinedChallengesViewModel()
         {
-            ChallengeItems = new[] {
+            joinedChallengeItems = new[] {
             new ChallengeItem("Challenge1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
                 " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
                 " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
@@ -22,6 +27,36 @@
                 " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories")};
         }
 
-        public ChallengeItem[] ChallengeItems { get; }
+        public void AllChallengesViewModel()
+        {
+            allChallengeItems = new[] {
+            new ChallengeItem("Challenge1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge2","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge3","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge4","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
+
+            new ChallengeItem("Challenge5","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories")};
+        }
+
+        public ChallengeItem[] AllChallengeItems
+        {
+            get { return allChallengeItems; }
+            set { }
+        }
+
+        public ChallengeItem[] JoinedChallengeItems
+        {
+            get { return joinedChallengeItems; }
+            set { }
+        }
     }
 }
