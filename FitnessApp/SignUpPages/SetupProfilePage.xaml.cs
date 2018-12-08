5846d52dae9c27f9c824758fa3537b8c47981b7b
Micheline Medhat <MichelineMedhat@gmail.com>
2018-12-08 20:01:42 +0200

    Add SetupProfilePage Code Behind
     * Include converting image to byte[] function.

--- FitnessApp/SignUpPages/SetupProfilePage.xaml.cs
+++ FitnessApp/SignUpPages/SetupProfilePage.xaml.cs
@@ -3,6 +3,10 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
+using FitnessApp.SQLdatabase;
+using System.IO;
+using System.Text.RegularExpressions;
+
 
 namespace FitnessApp.SignUpPages
 {
@@ -13,6 +17,9 @@ namespace FitnessApp.SignUpPages
     {
         public static SetUpProfilePage SetUpProfilePageObject = new SetUpProfilePage();
 
+        // Create an object from dataBase class
+        SQLqueries SQLqueriesObject = new SQLqueries();
+
         public SetUpProfilePage()
         {
             InitializeComponent();
@@ -21,11 +28,43 @@ namespace FitnessApp.SignUpPages
 
         private void SetUpProfileButton_Click(object sender, RoutedEventArgs e)
         {
-            UserMainWindow UserMainWindowTemp = new UserMainWindow();
-            SigningWindow.SigningWindowObject.Close();
-            UserMainWindowTemp.Show();
+            int heightLength = HeightTextBox.Text.Length;
+            int weightLength = WeightTextBox.Text.Length;
+            int targetWeightLength = TargetWeightTextBox.Text.Length;
+            int kilosToLosePerWeekLength = KilosToLosePerWeekTextBox.Text.Length;
+            int workoutsPerWeekLength = WorkoutsPerWeekTextBox.Text.Length;
+            int workoutHoursPerDayLength = WorkoutHoursPerDayTextBox.Text.Length;
+
+            // Constarins to make sure that all fields are filled with numbers
+            if (heightLength < 1 || weightLength < 1 || targetWeightLength < 1 || kilosToLosePerWeekLength < 1 || workoutsPerWeekLength < 1 || workoutHoursPerDayLength < 1)
+            {
+                SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("All fields are required!");
+            }
+
+            else
+            {
+                // Signing up
+                SQLqueriesObject.SignUp(ref img,
+                                        SignUpFirstPage.SignUpFirstPageObject.FirstNameTextBox.Text,
+                                        SignUpFirstPage.SignUpFirstPageObject.LastNameTextBox.Text,
+                                        SignUpFirstPage.SignUpFirstPageObject.UserNameTextBox.Text,
+                                        SignUpFirstPage.SignUpFirstPageObject.EmailTextBox.Text,
+                                        SignUpFirstPage.SignUpFirstPageObject.Password,
+                                        SignUpSecondPage.SignUpSecondPageObject.GenderComboBox.Text,
+                                        SignUpSecondPage.SignUpSecondPageObject.BirthDatePicker.Text,
+                                        double.Parse(WeightTextBox.Text), double.Parse(HeightTextBox.Text),
+                                        double.Parse(TargetWeightTextBox.Text),
+                                        double.Parse(KilosToLosePerWeekTextBox.Text),
+                                        int.Parse(WorkoutsPerWeekTextBox.Text),
+                                        int.Parse(WorkoutHoursPerDayTextBox.Text));
+
+                UserMainWindow UserMainWindowTemp = new UserMainWindow();
+                SigningWindow.SigningWindowObject.Close();
+                UserMainWindowTemp.Show();
+            }
         }
 
+        public byte[] img = null;
         private void ChooseImageButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog BrowseImageDialogBox = new OpenFileDialog();
@@ -37,12 +76,18 @@ namespace FitnessApp.SignUpPages
             {
                 UserProfilePhoto.ImageSource = new BitmapImage(new Uri(BrowseImageDialogBox.FileName));
                 UserProfilePhoto.Opacity = 1.0;
+
+                // Convert the image to byte[]
+                FileStream fs = new FileStream(BrowseImageDialogBox.FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                img = br.ReadBytes((int)fs.Length);
             }
         }
 
         private void TextBoxesNumbersOnly_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
