56969b6cd030853bcc9e1e597a52695ba491a4a0
Micheline Medhat <MichelineMedhat@gmail.com>
2018-12-08 19:55:42 +0200

    Add SignUpFirstPage Code Behind

--- FitnessApp/SignUpPages/SignUpFirstPage.xaml.cs
+++ FitnessApp/SignUpPages/SignUpFirstPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
-
+using FitnessApp.SQLdatabase;
 namespace FitnessApp.SignUpPages
 {
     /// <summary>
@@ -9,6 +9,9 @@ namespace FitnessApp.SignUpPages
     /// </summary>
     public partial class SignUpFirstPage : Page
     {
+        // creating an object from the sql class
+        SQLqueries objectSqlQueries = new SQLqueries();
+
         public static SignUpFirstPage SignUpFirstPageObject = new SignUpFirstPage();
 
         public SignUpFirstPage()
@@ -40,7 +43,58 @@ namespace FitnessApp.SignUpPages
             password = PasswordTextBox.Password;
             confirmedPassword = ConfirmPasswordTextBox.Password;
 
-            NavigationService.Navigate(SignUpSecondPage.SignUpSecondPageObject);
+            // Get the length of each input
+            int passwordLength = PasswordTextBox.Password.Length;
+            int firstNameLength = FirstNameTextBox.Text.Length;
+            int lastNameLength = LastNameTextBox.Text.Length;
+            int userNameLength = UserNameTextBox.Text.Length;
+            bool CheckUniqueUserName = objectSqlQueries.IsUsernameTaken(UserNameTextBox.Text);
+            bool CheckUniqueEmail = objectSqlQueries.IsEmailTaken(EmailTextBox.Text);
+            // Constraints , to make sure that texts boxes are not empty
+            if (firstNameLength < 1 || lastNameLength < 1 || userNameLength < 1 || passwordLength < 1)
+            {
+
+                SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("All fields are required!");
+            }
+            else
+            {
+                if (PasswordTextBox.Password == ConfirmPasswordTextBox.Password && passwordLength > 6 && CheckUniqueUserName == false && CheckUniqueEmail == false)
+                {
+                    if (EmailTextBox.Text.Contains("@"))
+                    {
+                        if (EmailTextBox.Text.Contains(".com"))
+                        {
+
+                            NavigationService.Navigate(SignUpSecondPage.SignUpSecondPageObject);
+                        }
+                    }
+                    else
+                    {
+                        SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("Invalid E-mail");
+                    }
+                }
+                else
+                {
+                    SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("password != confirmation password OR password < 7 ");
+                }
+
+
+                if (CheckUniqueUserName == true && CheckUniqueEmail == false)
+                {
+                    SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("User Name Is Already Taken ");
+
+                }
+
+                if (CheckUniqueEmail == true && CheckUniqueUserName == false)
+                {
+                    SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue(" Email Is Already Taken");
+                }
+                if (CheckUniqueEmail == true && CheckUniqueUserName == true)
+                {
+                    SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue(" Email and user name are already taken");
+                }
+
+            }
         }
     }
 }
