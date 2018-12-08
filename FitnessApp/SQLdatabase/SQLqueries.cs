7185c753cdf5e7238bf59eeee727cb8b857bfd4d
Micheline Medhat <MichelineMedhat@gmail.com>
2018-12-08 15:43:14 +0200

    Add SQLdatabase directory & SQLqueries class
     * Add SQLconnection string.

---
+++ FitnessApp/SQLdatabase/SQLqueries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.SQLdatabase
+{
+    class SQLqueries
+    {
+        // SQL Connection string.
+        // [IMPORTANT] Add your server name to data source.
+        SqlConnection Connection = new SqlConnection("data source=MICHA\\SQLEXPRESS; database=FITNESSAPP; integrated security=SSPI");
+
+    }
+}
