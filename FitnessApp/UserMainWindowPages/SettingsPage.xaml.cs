b3dcf12fb21b2ef24ad0cfd6ee35d60bc0b8bc30
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-12-08 04:11:32 +0200

    Open Required Expander only in SettingsPage
     * Add x:Name attribute to all expanders.
     * Add Expanded event handler to all expanders to close all unnecessary
       and open the required one only.
     * Initialize Profile Expander to be expanded from code behind instead
       of initializing it in SettingsPage.xaml file.

--- FitnessApp/UserMainWindowPages/SettingsPage.xaml.cs
+++ FitnessApp/UserMainWindowPages/SettingsPage.xaml.cs
@@ -13,11 +13,59 @@ namespace FitnessApp.UserMainWindowPages
         {
             InitializeComponent();
             SettingsPageObject = this;
+
+            // Initialize Profile Expander to be expanded
+            ProfileExpander.IsExpanded = true;
         }
 
         private void RatingBar_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<int> e)
         {
 
         }
+
+
+        private void Expander_Expanded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Remove Expanded Event Handler from all Expanders
+            // to prevent calling Expander_Expanded Event Handler 
+            // recursively.
+            ProfileExpander .Expanded -= Expander_Expanded;
+            AccountExpander .Expanded -= Expander_Expanded;
+            SecurityExpander.Expanded -= Expander_Expanded;
+            AboutExpander   .Expanded -= Expander_Expanded;
+            FeedbackExpander.Expanded -= Expander_Expanded;
+            HelpExpander    .Expanded -= Expander_Expanded;
+
+            // Close all Expanders.
+            ProfileExpander .IsExpanded = false;
+            AccountExpander .IsExpanded = false;
+            SecurityExpander.IsExpanded = false;
+            AboutExpander   .IsExpanded = false;
+            FeedbackExpander.IsExpanded = false;
+            HelpExpander    .IsExpanded = false;
+
+            // Re-add Expanded Event Handler to all Expanders.
+            ProfileExpander .Expanded += Expander_Expanded;
+            AccountExpander .Expanded += Expander_Expanded;
+            SecurityExpander.Expanded += Expander_Expanded;
+            AboutExpander   .Expanded += Expander_Expanded;
+            FeedbackExpander.Expanded += Expander_Expanded;
+            HelpExpander    .Expanded += Expander_Expanded;
+
+
+            // Get Current Expander object from sender.
+            Expander currentExpander = sender as Expander;
+
+            // Remove Expanded Event Handler from Current Expander.
+            currentExpander.Expanded -= Expander_Expanded;
+
+            // Open current Expander only.
+            currentExpander.IsExpanded = true;
+
+            // Re-add Expanded Event Handler to Current Expander.
+            currentExpander.Expanded += Expander_Expanded;
+
+        }
+
     }
 }
