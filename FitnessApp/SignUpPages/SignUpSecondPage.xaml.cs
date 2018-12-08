2374ac7ac240692fdf79eba2545d5c38dbc6ec2b
Micheline Medhat <MichelineMedhat@gmail.com>
2018-12-08 20:00:20 +0200

    Add SignUpSecondPage Code Behind

--- FitnessApp/SignUpPages/SignUpSecondPage.xaml.cs
+++ FitnessApp/SignUpPages/SignUpSecondPage.xaml.cs
@@ -19,12 +19,25 @@ namespace FitnessApp.SignUpPages
 
         private void SignUpButton_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(SetUpProfilePage.SetUpProfilePageObject);
+            // The length of each Box
+            int genderIsSelected = GenderComboBox.Text.Length;
+            int calenderIsSelected = BirthDatePicker.Text.Length;
 
-            //Change Back Card Header
-            SigningWindow.SigningWindowObject.BackArrowButton.Visibility = Visibility.Hidden;
-            SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Text = "Set up your Profile";
-            SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Margin = new Thickness(-15);
+            // Making sure that user filled all the requirments 
+            if (genderIsSelected < 4 || calenderIsSelected < 5 || (NotRobotCheckBox.IsChecked == false))
+            {
+                SigningWindow.SigningWindowObject.ErrorsSnackbar.MessageQueue.Enqueue("All fields are required!");
+            }
+
+            else
+            {
+                NavigationService.Navigate(SetUpProfilePage.SetUpProfilePageObject);
+
+                //Change Back Card Header
+                SigningWindow.SigningWindowObject.BackArrowButton.Visibility = Visibility.Hidden;
+                SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Text = "Set up your Profile";
+                SigningWindow.SigningWindowObject.BackCardHeaderTextBlock.Margin = new Thickness(-15);
+            }
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
