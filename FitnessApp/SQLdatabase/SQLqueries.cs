17707c394a79bdfcebde8806f8951706ad952b6c
Micheline Medhat <MichelineMedhat@gmail.com>
2018-12-08 16:59:10 +0200

    Add Sign Up Functions\Queries
     * Add Main Sign up function.
     * Add two functions to check whether the entered username and email are
       taken or not.

--- FitnessApp/SQLdatabase/SQLqueries.cs
+++ FitnessApp/SQLdatabase/SQLqueries.cs
@@ -112,5 +112,160 @@ namespace FitnessApp.SQLdatabase
             // If user doesn't exist or any other case, return false.
             return false; 
         }
+
+        // Check if the Username is taken or not; because Usernames must be unique
+        public bool IsUsernameTaken(string username)
+        {
+            // Create Command
+            SqlCommand cmd = new SqlCommand("select Username from [User] where Username=@username ;", Connection);
+            cmd.Parameters.AddWithValue("@username", username);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                if (dr.HasRows == true)
+                {
+                    Connection.Close();
+
+                    // If the Username is taken exists, return true.
+                    return true;
+                }
+
+            }
+
+            Connection.Close();
+
+            // If the Username is not taken, return false.
+            return false;
+        }
+
+        // Check if the Entered Email while signing up is taken or not
+        public bool IsEmailTaken(string email)
+        {
+            // Create Command
+            SqlCommand cmd = new SqlCommand("select Email from AdminAndUserAccount where Email=@email ;", Connection);
+            cmd.Parameters.AddWithValue("@email", email);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                if (dr.HasRows == true)
+                {
+                    Connection.Close();
+
+                    // If the email is taken exists, return true.
+                    return true;
+                }
+
+            }
+
+            Connection.Close();
+
+            // If the email is not taken, return false.
+            return false;
+        }
+
+        // Sign up function
+        public void SignUp(ref byte[] image,    string firstName,   string lastName,
+                           string username,     string email,       string password,
+                           string gender,       string birthDate,   double weight,         double height,
+                           double targetWeight, double kilosToLose, double workoutPerWeek, double workoutPerHours)
+        {
+            // Password Encryption
+            string encryptedPassword = PasswordEncryption(password);
+
+            // Create Query amd Command
+            string query1 = "INSERT INTO [User] (Image, FirstName, LastName, Username, BirthDate, Gender, "+
+                            "TargetWeight, Height, KilosToLosePerWeek, WorkoutsDaysPerWeek, WorkoutsHoursPerDay)" +
+                            "VALUES('" + image + "','" + firstName + "','" + lastName + "', '" + username + "','" + birthDate + "','" + gender + "','" + 
+                            targetWeight + "','" + height + "','" + kilosToLose + "','" + workoutPerWeek + "', '" + workoutPerHours + "') ;";
+            SqlCommand cmd1 = new SqlCommand(query1, Connection);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr;
+            dr = cmd1.ExecuteReader();
+
+            while (dr.Read())
+            {
+            }
+
+            Connection.Close();
+
+
+            // Get User's ID
+            // Create Query amd Command
+            string query2 = "Select ID from [User] where Username=@username";
+            SqlCommand cmd2 = new SqlCommand(query2, Connection);
+            cmd2.Parameters.AddWithValue("@username", username);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr2;
+            dr2 = cmd2.ExecuteReader();
+
+            while (dr2.Read())
+            {
+                if (dr2.HasRows == true)
+                {
+                    accountID = (int)dr2["ID"];
+                }
+
+            }
+
+            Connection.Close();
+
+
+            // Insert User's email and password
+            // Create Query amd Command
+            string query3 = "INSERT INTO AdminAndUserAccount(ID, Email, Password, Type" +
+                            "VALUES('" + accountID + "','" + email + "','" + encryptedPassword + "','User');";
+            SqlCommand cmd3 = new SqlCommand(query3, Connection);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr3;
+            dr3 = cmd3.ExecuteReader();
+
+            while (dr3.Read())
+            {
+            }
+
+            Connection.Close();
+
+
+            // Insert User's weight into multiValued weight table
+            // Create Query amd Command
+            string query4 = "INSERT INTO UserWeight(UserId,Weight,Date) " +
+                            "VALUES('" + accountID + "','" + weight + "', GETDATE());";
+            SqlCommand cmd4 = new SqlCommand(query4, Connection);
+
+            // Open Connection and Start Reading
+            Connection.Open();
+            SqlDataReader dr4;
+            dr4 = cmd4.ExecuteReader();
+
+            while (dr4.Read())
+            {
+            }
+
+            Connection.Close();
+
+
+            // Sending email to gmails only
+            if (email.Contains("gmail"))
+            {
+                SendEmail(email, firstName);
+            }
+        }
+
+
+
     }
 }
