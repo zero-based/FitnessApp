c681ee973be6ad4a38fbb035f34234dec6906371
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-09 22:48:10 +0200

    Update Challenges View Model
     * Update Arrays to be List<ChallengeModel>.
     * Use Loading Challenges Functions to initialize lists.
     * Add accountID Parameter to class functions.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- FitnessApp/ViewModels/ChallengesViewModel.cs
+++ FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -1,55 +1,38 @@
 ﻿using FitnessApp.Models;
+using System;
+using FitnessApp.SQLdatabase;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace FitnessApp.ViewModels
 {
     class ChallengesViewModel
     {
-        private ChallengeModel[] allChallengeModels;
-        private ChallengeModel[] joinedChallengeModels;
+        static SQLqueries SQLqueriesObject = new SQLqueries();
+        private List<ChallengeModel> allChallengeModels;
+        private List<ChallengeModel> joinedChallengeModels;
 
         public ChallengesViewModel() { }
 
-        public void AllChallengesViewModel()
+        public void AllChallengesViewModel(int accountID)
         {
-            allChallengeModels = new[] {
-            new ChallengeModel(100, true,"Swimming", "Challenge1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
-
-            new ChallengeModel(398, false,"Swimming", "Challenge2","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
-
-            new ChallengeModel(999, false,"Swimming", "Challenge3","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
-
-            new ChallengeModel(990, true,"Swimming", "Challenge4","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
-
-            new ChallengeModel(558, false,"Swimming", "Challenge5","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories")};
-
+            allChallengeModels = SQLqueriesObject.LoadAllChallenges(accountID);
         }
 
-        public void JoinedChallengesViewModel()
+        public void JoinedChallengesViewModel(int accountID)
         {
-            joinedChallengeModels = new[] {
-            new ChallengeModel(100, true,"Swimming", "Challenge1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories"),
-
-            new ChallengeModel(990, true,"Swimming", "Challenge4","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM", "Lose 1500 Calories")};
+            joinedChallengeModels = SQLqueriesObject.LoadJoinedChallenges(accountID);
         }
 
-        public ChallengeModel[] AllChallengeModels
+        public List<ChallengeModel> AllChallengeModels
         {
-            get { return allChallengeModels; }
+            get => allChallengeModels;
             set { }
         }
 
-        public ChallengeModel[] JoinedChallengeModels
+        public List<ChallengeModel> JoinedChallengeModels
         {
-            get { return joinedChallengeModels; }
+            get => joinedChallengeModels;
             set { }
         }
     }
