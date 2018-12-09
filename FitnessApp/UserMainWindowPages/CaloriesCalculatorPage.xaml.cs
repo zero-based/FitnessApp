47f7b6a22ef3861eb5b671fa9bfbc0759f4adea2
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-09 05:08:17 +0200

    Add Core Calculations to Calories Calculator
    Co-authored-by: Monica Atef <monicaatef46@gmail.com>

--- FitnessApp/UserMainWindowPages/CaloriesCalculatorPage.xaml.cs
+++ FitnessApp/UserMainWindowPages/CaloriesCalculatorPage.xaml.cs
@@ -17,7 +17,29 @@ namespace FitnessApp.UserMainWindowPages
 
         private void CalculateButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            if (GenderComboBox.Text == ("Female"))
+            {
 
+                double femaleCalculate;
+                double weight = double.Parse(WeightTextBox.Text);
+                double height = double.Parse(HeightTextBox.Text);
+                double age = double.Parse(AgeTextBox.Text);
+                femaleCalculate = 665 + (9.6 * (weight)) + (1.8 * (height)) - (4.7 * (age));
+                ResultTextBlock.Text = femaleCalculate.ToString() + " kCal.";
+
+            }
+            else
+            {
+
+                double maleCalculate;
+                double weight = double.Parse(WeightTextBox.Text);
+                double height = double.Parse(HeightTextBox.Text);
+                double age = double.Parse(AgeTextBox.Text);
+                maleCalculate = 66 + (13.7 * (weight)) + (1.8 * (height)) - (4.7 * (age));
+                ResultTextBlock.Text = maleCalculate.ToString() + " kCal.";
+
+            }
         }
+
     }
 }
