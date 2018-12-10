38713f7d8a12c85948c9c0c24aa571c7a531dcdf
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-10 23:06:28 +0200

    Update Plans View Model
     * Update Array to be List<PlanModel>.
     * Use LoadPlan Function to initialize list.
     * Add accountID Parameter to class constructor.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- FitnessApp/ViewModels/PlansViewModel.cs
+++ FitnessApp/ViewModels/PlansViewModel.cs
@@ -1,24 +1,24 @@
 ﻿using FitnessApp.Models;
+using FitnessApp.SQLdatabase;
+using System.Collections.Generic;
 
 namespace FitnessApp.ViewModels
 {
     class PlansViewModel
     {
-        public PlansViewModel()
+        static SQLqueries SQLqueriesObject = new SQLqueries();
+        private List<PlanModel> planModels;
+
+        public PlansViewModel(int accountID)
         {
-            PlanModels = new[] {
-        new PlanModel("Plan1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-            " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
-            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM"),
-
-        new PlanModel("Plan2","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM"),
-
-        new PlanModel("Plan3","Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-            " ullamco laboris nisi ut aliquip ex ea.", "30-Nov-18", "Run 10 KM")
-            };
+            planModels = SQLqueriesObject.LoadPlans(accountID);
         }
-        public PlanModel[] PlanModels { get; }
+
+        public List<PlanModel> PlanModels
+        {
+            get => planModels;
+            set { }
+        }
     }
 }
 
