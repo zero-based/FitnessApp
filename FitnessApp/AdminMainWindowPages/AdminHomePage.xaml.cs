64c119969216439452920c6734449de41f911842
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-13 00:37:54 +0200

    Add AdminHomePage.xaml.cs
     * Made instances of AdminHomePageObject and SQLqueriesOject to use
       later.

---
+++ FitnessApp/AdminMainWindowPages/AdminHomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FitnessApp.SQLdatabase;
+using System.Windows.Controls;
+
+namespace FitnessApp.AdminMainWindowPages
+{
+    /// <summary>
+    /// Interaction logic for AdminHomePage.xaml
+    /// </summary>
+    public partial class AdminHomePage : Page
+    {
+        public static AdminHomePage AdminHomePageObject = new AdminHomePage();
+        SQLqueries SQLqueriesObject = new SQLqueries();
+
+        public AdminHomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
