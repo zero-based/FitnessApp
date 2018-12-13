d7159e538b73cb5e261428a70baadbdbd4ce3c92
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-13 19:31:10 +0200

    Add Feedback Model and ViewModel
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

---
+++ FitnessApp/ViewModels/FeedbacksViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FitnessApp.Models;
+using System.Collections.Generic;
+
+namespace FitnessApp.ViewModels
+{
+    class FeedbacksViewModel
+    {
+        private List<FeedbackModel> feedbackModels;
+        public FeedbacksViewModel()
+        {
+            feedbackModels = new List<FeedbackModel>()
+            {
+                new FeedbackModel() { UserName = "JHON DOE", Feedback = "Lorem iposem" },
+                new FeedbackModel() { UserName = "JHON DOE", Feedback = "Lorem iposem" },
+                new FeedbackModel() { UserName = "JHON DOE", Feedback = "Lorem iposem" }
+            };
+        }
+        public List<FeedbackModel> FeedbackModels {  get => feedbackModels; set { } }
+    }
+}
