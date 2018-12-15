1275dbeff50493634edf7554f5c3916f7d9b2013
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-12-15 04:42:31 +0200

    Update ServerDetails Class
     * Change ConnectionString to auto property.

--- FitnessApp/SQLdatabase/ServerDetails.cs
+++ FitnessApp/SQLdatabase/ServerDetails.cs
@@ -4,13 +4,16 @@
     {
 
         // Enter Server Name and Database Name Here
-        public const string server   = @"Server_Name_Here";
+        public const string server = @"Server_Name_Here";
         public const string database = "FITNESSAPP";
         public const string security = "SSPI";
 
         // Initialize Connection String
-        public const string ConnectionString =  "data source = "         + server   + "; " +
-                                                "database = "            + database + "; " +
-                                                "integrated security = " + security;
+        public static string ConnectionString
+        {
+            get { return "data source = " + server + "; " + "database = " + database + "; " + "integrated security = " + security; }
+            set { ConnectionString = value; }
+        }
+
     }
 }
