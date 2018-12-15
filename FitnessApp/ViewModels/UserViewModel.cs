f100ba4c3a94da07bcf5a464031856496c606d57
Michael Safwat <michaelsafwat.hanna@gmail.com>
2018-12-15 23:02:59 +0200

    Add User View Model
     * Create a view model class for User and initialize it with
       static temporary arguments.
     * userModels List will be initialized later from Database depending on
       Admin's search input.

---
+++ FitnessApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FitnessApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.ViewModels
+{
+    class UserViewModel
+    {
+        private List<UserModel> userModels;
+
+        public UserViewModel()
+        {
+            userModels = new List<UserModel>()
+            {
+                new UserModel() { FirstName ="JHON DOE", Email="JHON DOE@LoremIposem.com" }
+            };
+        }
+
+
+        public List<UserModel> UserModels { get => userModels; set { } }
+
+    }
+}
