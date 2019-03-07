122680b1b93846909d142f7c448ca55bac90d40f
Youssef Raafat <youssefraafatnasry@gmail.com>
2019-03-08 01:56:45 +0200

    Add ServerDetails.cs

---
+++ FitnessApp/SQLserver/ServerDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace FitnessApp.SQLserver
+{
+    public static class ServerDetails
+    {
+
+        // Enter Server Name and Database Name Here
+        public const string server = @"server_name_here";
+        public const string database = "FITNESSAPP";
+        public const string security = "SSPI";
+
+        // Initialize Connection string
+        public static string ConnectionString
+        {
+            get { return "data source = " + server + "; " + "database = " + database + "; " + "integrated security = " + security; }
+            set { ConnectionString = value; }
+        }
+
+    }
+}
